c55e468d1fa95998d8a37fe19925a0f2f05e0c48
Dysthesis <antheoraviel@protonmail.com>
2024-03-09 17:02:10 +1100

    Changes.

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -7,25 +7,9 @@
         enable = true;
         settings.formatting.command = "${pkgs.alejandra}/bin/alejandra";
       };
+
       rust-analyzer = {
         enable = true;
-        installCargo = true;
-        installRustc = true;
-        settings = {
-          checkOnSave = true;
-          check = {
-            command = "clippy";
-          };
-          # inlayHints = {
-          #   enable = true;
-          #   showParameterNames = true;
-          #   parameterHintsPrefix = "<- ";
-          #   otherHintsPrefix = "=> ";
-          # };
-          procMacro = {
-            enable = true;
-          };
-        };
       };
     };
     onAttach = ''
