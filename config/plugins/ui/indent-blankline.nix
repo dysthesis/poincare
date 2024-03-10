d0cf691f2b073d9a84576f3c9b68fc7732ea0cad
Dysthesis <antheoraviel@protonmail.com>
2024-03-10 18:04:57 +1100

    Changes

--- config/plugins/ui/indent-blankline.nix
+++ config/plugins/ui/indent-blankline.nix
@@ -2,9 +2,25 @@
   plugins.indent-blankline = {
     enable = true;
     indent.char = "│";
-  };
-  scope = {
-    enabled = true;
-    showStart = true;
+    scope = {
+      enabled = true;
+      showStart = true;
+    };
+    exclude = {
+      buftypes = ["terminal" "nofile"];
+      filetypes = [
+        "help"
+        "alpha"
+        "dashboard"
+        "neo-tree"
+        "Trouble"
+        "trouble"
+        "lazy"
+        "mason"
+        "notify"
+        "toggleterm"
+        "lazyterm"
+      ];
+    };
   };
 }
