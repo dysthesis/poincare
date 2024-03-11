0d4cf65215212015d2018901a6f2f4a592b46ee8
Dysthesis <antheoraviel@protonmail.com>
2024-03-12 08:22:56 +1100

    Yet another attempt at fixing nvim-cmp

--- config/plugins/completion/nvim-cmp.nix
+++ config/plugins/completion/nvim-cmp.nix
@@ -14,7 +14,24 @@
 
         mapping = {
           "<Return>" = "cmp.mapping.confirm({ select = true }, {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<Tab>" = {
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expandable() then
+                  luasnip.expand()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif check_backspace() then
+                  fallback()
+                else
+                  fallback()
+                end
+              end
+            '';
+            modes = ["i" "s"];
+          };
         };
 
         sources = [
