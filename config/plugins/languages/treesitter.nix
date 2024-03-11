a9fd7c21504cb3f9f9ce3417eb4d35becf05fc8c
Dysthesis <antheoraviel@protonmail.com>
2024-03-12 07:21:24 +1100

    Enable treesitter-context

--- config/plugins/languages/treesitter.nix
+++ config/plugins/languages/treesitter.nix
@@ -1,9 +1,12 @@
 {
-  plugins.treesitter = {
-    enable = true;
-    nixGrammars = true;
-    indent = true;
-    # folding = true;
-    nixvimInjections = true;
+  plugins = {
+    treesitter = {
+      enable = true;
+      nixGrammars = true;
+      indent = true;
+      # folding = true;
+      nixvimInjections = true;
+    };
+    treesitter-context.enable = true;
   };
 }
