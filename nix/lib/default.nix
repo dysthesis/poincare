532e61c25da099f6b990e0f3f8b993cce9647aaf
Dysthesis <antheoraviel@protonmail.com>
2025-02-09 18:03:30 +1100

    More lib

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -2,5 +2,7 @@ inputs:
 inputs.nixpkgs.lib.extend (_final: _prev: {
   poincare = {
     forAllSystems = import ./forAllSystems.nix inputs.nixpkgs;
+    mkNeovim = import ./mkNeovim.nix;
+    mkPlugin = import ./mkPlugin.nix _final;
   };
 })
