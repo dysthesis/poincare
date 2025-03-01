0a94be97c26319c2481b63c4245cffdbde10191f
Dysthesis <antheoraviel@protonmail.com>
2025-03-01 20:52:23 +1100

    Missing some keybinds in smart-splits.nvim

--- lua/plugins/smart-splits-nvim.lua
+++ lua/plugins/smart-splits-nvim.lua
@@ -30,6 +30,34 @@ require('lz.n').load {
       desc = 'Resize right',
     },
     {
+      '<C-h>',
+      function()
+        require('smart-splits').move_cursor_left()
+      end,
+      desc = 'Move cursor left',
+    },
+    {
+      '<C-j>',
+      function()
+        require('smart-splits').move_cursor_down()
+      end,
+      desc = 'Move cursor down',
+    },
+    {
+      '<C-k>',
+      function()
+        require('smart-splits').move_cursor_up()
+      end,
+      desc = 'Move cursor up',
+    },
+    {
+      '<C-l>',
+      function()
+        require('smart-splits').move_cursor_right()
+      end,
+      desc = 'Move cursor right',
+    },
+    {
       '<C-\\>',
       function()
         require('smart-splits').move_cursor_previous()
