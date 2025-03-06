2a435cfd21c5ce7017d0610c5dbadde49cedcf33
Dysthesis <antheoraviel@protonmail.com>
2025-03-06 13:15:05 +1100

    Fixes

--- lua/plugins/markview-nvim.lua
+++ lua/plugins/markview-nvim.lua
@@ -3,10 +3,12 @@ require('lz.n').load {
   ft = 'markdown',
   after = function()
     require('markview').setup {
-      headings = require('markview.presets').headings.glow,
-      code_blocks = { hl = 'CursorColumn' },
+      markdown = {
+        list_items = { shift_width = 2 },
+        headings = require('markview.presets').headings.glow,
+      },
       inline_codes = { hl = 'CursorColumn' },
-      list_items = { shift_width = 2 },
+      code_blocks = { border_hl = 'CursorColumn' },
     }
   end,
 }
