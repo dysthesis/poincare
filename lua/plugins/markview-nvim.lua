d6f86753a3d13e80bfbfa3a41fbef39d5ca04174
Dysthesis <antheoraviel@protonmail.com>
2025-03-06 13:15:41 +1100

    Changes

--- lua/plugins/markview-nvim.lua
+++ lua/plugins/markview-nvim.lua
@@ -5,7 +5,65 @@ require('lz.n').load {
     require('markview').setup {
       markdown = {
         list_items = { shift_width = 2 },
-        headings = require('markview.presets').headings.glow,
+        headings = {
+          ---+ ${conf, Glow-like headings}
+          enable = true,
+          shift_width = 0,
+
+          heading_1 = {
+            style = 'label',
+            sign = '󰌕 ',
+            sign_hl = 'MarkviewHeading1Sign',
+
+            padding_left = ' ',
+            padding_right = ' ',
+            icon = '󰫈 ',
+            hl = 'MarkviewHeading1',
+          },
+          heading_2 = {
+            style = 'label',
+            sign = '󰌖 ',
+            sign_hl = 'MarkviewHeading2Sign',
+
+            padding_left = ' ',
+            padding_right = ' ',
+            icon = '󰫇 ',
+            hl = 'MarkviewHeading2',
+          },
+          heading_3 = {
+            style = 'label',
+
+            padding_left = ' ',
+            padding_right = ' ',
+            icon = '󰫆 ',
+            hl = 'MarkviewHeading3',
+          },
+          heading_4 = {
+            style = 'label',
+
+            padding_left = ' ',
+            padding_right = ' ',
+            icon = '󰫅 ',
+            hl = 'MarkviewHeading4',
+          },
+          heading_5 = {
+            style = 'label',
+
+            padding_left = ' ',
+            padding_right = ' ',
+            icon = '󰫄 ',
+            hl = 'MarkviewHeading5',
+          },
+          heading_6 = {
+            style = 'label',
+
+            padding_left = ' ',
+            padding_right = ' ',
+            icon = '󰫃 ',
+            hl = 'MarkviewHeading6',
+          },
+          ---_
+        },
       },
       inline_codes = { hl = 'CursorColumn' },
       code_blocks = { border_hl = 'CursorColumn' },
