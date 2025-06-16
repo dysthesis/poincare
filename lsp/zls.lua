e416a25b232f024b251a58a034da0fa28b875d44
Dysthesis <antheoraviel@protonmail.com>
2025-06-16 13:55:12 +1000

    Typo

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -1,6 +1,6 @@
 return {
-  filetypes = { 'zig' },
-  cmd = { 'zls', 'zon' },
+  filetypes = { 'zig', 'zon' },
+  cmd = { 'zls' },
   root_markers = { 'build.zig', 'build.zig.zon', '.git' },
   settings = {
     zls = {
