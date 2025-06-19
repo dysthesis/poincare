0e80be020c1fec4bb61b9653695df63dd4980a95
Dysthesis <antheoraviel@protonmail.com>
2025-06-19 10:22:43 +1000

    Debugging keybinds

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,5 +1,6 @@
 ---@return rustaceanvim.Opts
 vim.g.rustaceanvim = function()
+  local bufnr = vim.api.nvim_get_current_buf()
   ---@type rustaceanvim.Opts
   local rustacean_opts = {
     server = {
@@ -25,5 +26,25 @@ vim.g.rustaceanvim = function()
       adapter = require('rustaceanvim.config').get_codelldb_adapter(vim.g.codelldb_path, vim.g.liblldb_path),
     },
   }
+  vim.keymap.set(
+    'n',
+    'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+    function()
+      vim.cmd.RustLsp { 'hover', 'actions' }
+    end,
+    { silent = true, buffer = bufnr }
+  )
+
+  vim.keymap.set('n', '<leader>dc', function()
+    vim.cmd.RustLsp('debuggables')
+  end, { silent = true, buffer = bufnr })
+
+  vim.keymap.set('n', '<leader>dr', function()
+    vim.cmd.RustLsp('renderDiagnostic')
+  end, { silent = true, buffer = bufnr })
+
+  vim.keymap.set('n', '<leader>de', function()
+    vim.cmd.RustLsp('explainError')
+  end, { silent = true, buffer = bufnr })
   return rustacean_opts
 end
