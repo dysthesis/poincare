03a674e3ff8d560f7181e7d4141d3bb41176c4bb
Dysthesis <antheoraviel@protonmail.com>
2025-07-12 17:20:59 +1000

    Cleanup

--- src/lua/utils/format.lua
+++ src/lua/utils/format.lua
@@ -1,28 +1,16 @@
-vim.api.nvim_create_augroup('AutoFormat', {})
+vim.api.nvim_create_augroup('AutoFormat', { clear = true })
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-  pattern = '**.nix',
-  group = 'AutoFormat',
-  callback = function()
-    vim.cmd('silent !alejandra -qq %')
-    vim.cmd('edit')
-  end,
-})
+local function register_formatter(ft, cmd)
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    pattern = '**.' .. ft,
+    group = 'AutoFormat',
+    callback = function()
+      vim.cmd('silent !' .. cmd)
+      vim.cmd('edit')
+    end,
+  })
+end
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-  pattern = '**.rs',
-  group = 'AutoFormat',
-  callback = function()
-    vim.cmd('silent !cargo fmt')
-    vim.cmd('edit')
-  end,
-})
-vim.api.nvim_create_autocmd('BufWritePost', {
-  pattern = '**.lua',
-  group = 'AutoFormat',
-  callback = function()
-    local name = vim.api.nvim_buf_get_name(0)
-    vim.cmd(':silent :!stylua ' .. name)
-  end,
-  group = autocmd_group,
-})
+register_formatter('nix', 'alejandra -qq %')
+register_formatter('rs', 'cargo fmt')
+register_formatter('lua', 'stylua %')
