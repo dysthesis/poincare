bf02e535b3fc0172ebddba55bba8af4100b41919
Dysthesis <antheoraviel@protonmail.com>
2025-07-12 19:35:23 +1000

    Autoformatter works

--- nix/packages/poincare.nix
+++ nix/packages/poincare.nix
@@ -9,14 +9,15 @@
   vimUtils,
   lib,
   ...
-}: let
+}:
+let
   sources = import ./npins;
-  npins = lib.mapAttrs (k: _v: import sources.${k} {}) sources;
+  npins = lib.mapAttrs (k: _v: import sources.${k} { }) sources;
   packageName = "plugins";
 
   config = vimUtils.buildVimPlugin {
     name = "poincare";
-    src = runCommand "poincare-config-src" {} ''
+    src = runCommand "poincare-config-src" { } ''
       mkdir -p $out/plugin
       cp -r ${self}/src/init.lua $out/plugin/
       cp -r ${self}/src/lua $out/
@@ -24,17 +25,18 @@
     doCheck = false;
   };
 
-  plugins = with vimPlugins;
-  with npins; [
-    config
-    lz-n
-    smart-splits-nvim
-    leap-nvim
-    mini-surround
-    nvim-treesitter-textobjects
-    (nvim-treesitter.withPlugins (
-      p:
-        with p; [
+  plugins =
+    with vimPlugins;
+    with npins;
+    [
+      config
+      lz-n
+      smart-splits-nvim
+      leap-nvim
+      mini-surround
+      nvim-treesitter-textobjects
+      (nvim-treesitter.withPlugins (
+        p: with p; [
           go
           bash
           fish
@@ -57,31 +59,30 @@
           latex
           typst
         ]
-    ))
-    lackluster-nvim
-  ];
-  packPath = runCommandLocal "packpath" {} ''
+      ))
+      lackluster-nvim
+    ];
+  packPath = runCommandLocal "packpath" { } ''
     mkdir -p $out/pack/${packageName}/{start,opt}
     ${lib.concatMapStringsSep "\n" (
-        plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}"
-      )
-      plugins}
+      plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}"
+    ) plugins}
   '';
 in
-  symlinkJoin rec {
-    name = "poincare";
-    paths = [neovim-unwrapped];
-    nativeBuildInputs = [makeWrapper];
-    postBuild = ''
-      wrapProgram $out/bin/nvim \
-        --add-flags "-u" \
-        --add-flags "NORC" \
-        --add-flags "--cmd" \
-        --add-flags "'set packpath^=${packPath} | set runtimepath^=${packPath}'" \
-        --set-default NVIM_APPNAME ${name}
-    '';
-    passthru = {
-      inherit packPath;
-    };
-    meta.mainProgram = "nvim";
-  }
+symlinkJoin rec {
+  name = "poincare";
+  paths = [ neovim-unwrapped ];
+  nativeBuildInputs = [ makeWrapper ];
+  postBuild = ''
+    wrapProgram $out/bin/nvim \
+      --add-flags "-u" \
+      --add-flags "NORC" \
+      --add-flags "--cmd" \
+      --add-flags "'set packpath^=${packPath} | set runtimepath^=${packPath}'" \
+      --set-default NVIM_APPNAME ${name}
+  '';
+  passthru = {
+    inherit packPath;
+  };
+  meta.mainProgram = "nvim";
+}
--- src/lua/utils/format.lua
+++ src/lua/utils/format.lua
@@ -1,16 +1,39 @@
-vim.api.nvim_create_augroup('AutoFormat', { clear = true })
+local fmt_by_ft = {
+  nix = function(buf)
+    return { 'alejandra', '-qq', vim.api.nvim_buf_get_name(buf) }
+  end,
+  lua = function(buf)
+    return { 'stylua', vim.api.nvim_buf_get_name(buf) }
+  end,
+  sh = function(buf)
+    return { 'shfmt', '-w', '-i', '2', '-ci', vim.api.nvim_buf_get_name(buf) }
+  end,
+  rust = function()
+    return { 'cargo', 'fmt' }
+  end,
+}
 
-local function register_formatter(ft, cmd)
-  vim.api.nvim_create_autocmd('BufWritePost', {
-    pattern = '**.' .. ft,
-    group = 'AutoFormat',
-    callback = function()
-      vim.cmd('silent !' .. cmd)
-      vim.cmd('edit')
-    end,
-  })
-end
+local group = vim.api.nvim_create_augroup('AutoFormat', { clear = true })
 
-register_formatter('nix', 'alejandra -qq %')
-register_formatter('rs', 'cargo fmt')
-register_formatter('lua', 'stylua %')
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = group,
+  pattern = '*',
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    local make = fmt_by_ft[ft]
+    if not make then
+      return
+    end
+    vim.system(
+      make(args.buf),
+      { text = true },
+      vim.schedule_wrap(function(res)
+        if res.code == 0 then
+          vim.cmd('silent checktime ' .. args.buf)
+        else
+          vim.notify('Formatter failed with code: ' .. res.code, vim.log.levels.WARN)
+        end
+      end)
+    )
+  end,
+})
