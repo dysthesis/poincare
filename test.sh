0301d8a2243accf032bdeec4e1f935698e03131c
Dysthesis <antheoraviel@protonmail.com>
2025-07-13 09:46:55 +1000

    Basic script to colourise rg results

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+rg --files --hidden --follow --glob '!.git/*' | while IFS= read -r path; do
+  # We remove everything from the beginning (`##`) up to and including the last slash (`*/`) to
+  # get the basename of the path.
+  basename="${path##*/}"
+
+  # We remove the last slash and everything after it (`%/*`) from the end to get the dirname of the
+  # path.
+  dirname="${path%/*}"
+
+  # There are two cases to handle here, namely:
+  #
+  # - the path has no dirname, _i.e.,_ dirname == path, and
+  # - the path has a dirname.
+  echo "$(tput setaf 244)$dirname/$(tput sgr0)$(tput bold)$basename$(tput sgr0)"
+done
