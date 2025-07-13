77c6ce9836911e1c3790c2422508abc235e986f1
Dysthesis <antheoraviel@protonmail.com>
2025-07-13 14:45:34 +1000

    Formatting

--- src/lua/utils/picker/file.lua
+++ src/lua/utils/picker/file.lua
@@ -1,14 +1,14 @@
-local picker = require("utils.picker.core")
+local picker = require('utils.picker.core')
 
 local M = {}
 
 local function strip_ansi(s)
-	return (s:gsub("\27%[[0-9;]*m", ""))
+  return (s:gsub('\27%[[0-9;]*m', ''))
 end
 
 M.open = function()
-	picker.run({
-		producer = [=[
+  picker.run {
+    producer = [=[
       rg --files --hidden --follow --glob '!.git/*' | while IFS= read -r path; do
         basename="${path##*/}"
         dirname="${path%/*}"
@@ -20,21 +20,21 @@ M.open = function()
         fi
       done \
     ]=],
-		preview = "bat --style=numbers --color=always {}",
-		parse = function(lines)
-			local out = {}
-			for _, l in ipairs(lines) do
-				table.insert(out, strip_ansi(l))
-			end
-			return out
-		end,
-		sink = function(paths, key)
-			local cmd = ({ ["ctrl-v"] = "vsplit" })[key] or "edit"
-			for _, p in ipairs(paths) do
-				vim.cmd(cmd .. " " .. vim.fn.fnameescape(p))
-			end
-		end,
-	})
+    preview = 'bat --style=numbers --color=always {}',
+    parse = function(lines)
+      local out = {}
+      for _, l in ipairs(lines) do
+        table.insert(out, strip_ansi(l))
+      end
+      return out
+    end,
+    sink = function(paths, key)
+      local cmd = ({ ['ctrl-v'] = 'vsplit' })[key] or 'edit'
+      for _, p in ipairs(paths) do
+        vim.cmd(cmd .. ' ' .. vim.fn.fnameescape(p))
+      end
+    end,
+  }
 end
 
 return M
