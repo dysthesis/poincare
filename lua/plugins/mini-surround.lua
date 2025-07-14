9905408eaa82a6d1a5064f844ba169ebaf926d21
Dysthesis <antheoraviel@protonmail.com>
2025-07-14 18:10:15 +1000

    Try better remappping

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -4,13 +4,9 @@ require('lz.n').load {
   after = function()
     require('mini.surround').setup {
       mappings = {
-        add = 'Sa', -- Add surrounding in Normal and Visual modes
-        delete = 'Sd', -- Delete surrounding
-        find = 'Sf', -- Find surrounding (to the right)
-        find_left = 'SF', -- Find surrounding (to the left)
-        highlight = 'Sh', -- Highlight surrounding
-        replace = 'Sr', -- Replace surrounding
-        update_n_lines = 'Sn', -- Update `n_lines`
+        add = 's', -- Add surrounding in Normal and Visual modes
+        delete = 'ds', -- Delete surrounding
+        replace = 'cs', -- Replace surrounding
       },
     }
   end,
