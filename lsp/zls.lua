3dc609481ada2ae0287ad695317ff63f9e2c3be6
Dysthesis <antheoraviel@protonmail.com>
2025-10-11 13:45:39 +1100

    Changes

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -6,8 +6,6 @@ return {
     zls = {
       enable_build_on_save = true,
       build_on_save_step = 'check',
-      -- Neovim already provides basic syntax highlighting
-      semantic_tokens = 'partial',
     },
   },
 }
