ba745eb3c2751cc47f1f85a778f6b38becbe55eb
Dysthesis <antheoraviel@protonmail.com>
2025-10-16 09:31:21 +1100

    Changes

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -6,4 +6,7 @@
 -- map('n', '<leader>or', function()
 --   require('utils.references').open_reference(0)
 -- end, { buffer = true, silent = true, desc = 'Open front-matter reference' })
-vim.opt.smoothscroll = true
+vim.opt_local.wrap = true
+vim.opt_local.smoothscroll = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
