076ec39bf2a11adb75a21cded6b36f3ebe82b689
Dysthesis <antheoraviel@protonmail.com>
2025-10-19 17:44:39 +1100

    Buffer lines picker

--- lua/plugins/mini-pick.lua
+++ lua/plugins/mini-pick.lua
@@ -17,6 +17,13 @@ require('lz.n').load {
       desc = 'Find [G]rep',
     },
     {
+      '<leader>l',
+      function()
+        require('mini.pick').registry.buffer_lines_current()
+      end,
+      desc = 'Find buffer [L]ines',
+    },
+    {
       '<leader>d',
       function()
         require('mini.extra').pickers.diagnostic()
@@ -37,6 +44,7 @@ require('lz.n').load {
       end,
       desc = 'Find [D]iagnostics',
     },
+
     {
       '<leader>s',
       function()
@@ -75,6 +83,48 @@ require('lz.n').load {
   },
   after = function()
     local MiniPick = require('mini.pick')
+    local MiniExtra = require('mini.extra')
+
+    local ns_digit_prefix = vim.api.nvim_create_namespace('cur-buf-pick-show')
+    local show_cur_buf_lines = function(buf_id, items, query, opts)
+      if items == nil or #items == 0 then
+        return
+      end
+
+      -- Show as usual
+      MiniPick.default_show(buf_id, items, query, opts)
+
+      -- Move prefix line numbers into inline extmarks
+      local lines = vim.api.nvim_buf_get_lines(buf_id, 0, -1, false)
+      local digit_prefixes = {}
+      for i, l in ipairs(lines) do
+        local _, prefix_end, prefix = l:find('^(%s*%d+│)')
+        if prefix_end ~= nil then
+          digit_prefixes[i], lines[i] = prefix, l:sub(prefix_end + 1)
+        end
+      end
+
+      vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
+      for i, pref in pairs(digit_prefixes) do
+        local opts = { virt_text = { { pref, 'MiniPickNormal' } }, virt_text_pos = 'inline' }
+        vim.api.nvim_buf_set_extmark(buf_id, ns_digit_prefix, i - 1, 0, opts)
+      end
+
+      -- Set highlighting based on the curent filetype
+      local ft = vim.bo[items[1].bufnr].filetype
+      local has_lang, lang = pcall(vim.treesitter.language.get_lang, ft)
+      local has_ts, _ = pcall(vim.treesitter.start, buf_id, has_lang and lang or ft)
+      if not has_ts and ft then
+        vim.bo[buf_id].syntax = ft
+      end
+    end
+
+    MiniPick.registry.buffer_lines_current = function()
+      -- local local_opts = { scope = "current", preserve_order = true } -- use preserve_order
+      local local_opts = { scope = 'current' }
+      MiniExtra.pickers.buf_lines(local_opts, { source = { show = show_cur_buf_lines } })
+    end
+
     MiniPick.setup {
       options = {
         use_cache = true,
