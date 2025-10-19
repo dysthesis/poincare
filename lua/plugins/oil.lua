d9ddad0789d29d020eafab3aaeb9588f30d5e35b
Dysthesis <antheoraviel@protonmail.com>
2025-10-19 13:48:55 +1100

    Oil keybinds

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,10 +2,21 @@ require('lz.n').load {
   'oil.nvim',
   cmd = 'Oil',
   event = { 'VimEnter */*,.*', 'BufNew */*,.*' },
+  keys = {
+    {
+      '<leader>.',
+      '<cmd>Oil<cr>',
+      'Open current working directory',
+    },
+  },
   after = function()
     require('oil').setup {
-      view_options = {
-        show_hidden = true,
+      skip_confirm_for_simple_edits = true,
+      columns = {
+        'icon',
+        'permissions',
+        'size',
+        'mtime',
       },
     }
   end,
