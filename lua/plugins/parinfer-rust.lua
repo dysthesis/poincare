eac7e5783d6eebf95082ee8e97baf500f8cea42a
Dysthesis <antheoraviel@protonmail.com>
2025-11-02 10:03:52 +1100

    Configure parinfer-rust

---
+++ lua/plugins/parinfer-rust.lua
@@ -0,0 +1,16 @@
+require('lz.n').load {
+  'parinfer-rust',
+  ft = {
+    'clojure',
+    'scheme',
+    'lisp',
+    'racket',
+    'hy',
+    'fennel',
+    'janet',
+    'carp',
+    'wast',
+    'yuck',
+    'dune',
+  },
+}
