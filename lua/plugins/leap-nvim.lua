3ddb80ec2c5139d1334a5d7d98be017ae2c20115
Dysthesis <antheoraviel@protonmail.com>
2025-12-26 09:45:39 +1100

    Fix leap deprecated function

--- lua/plugins/leap-nvim.lua
+++ lua/plugins/leap-nvim.lua
@@ -1,5 +1,5 @@
 -- Leap lazy loads itself
-require('leap').create_default_mappings()
+require('leap').leap { backward = true }
 -- If using the default mappings (`gs` for multi-window mode), you can
 -- map e.g. `gS` here.
 vim.keymap.set({ 'n', 'x', 'o' }, 'gs', function()
