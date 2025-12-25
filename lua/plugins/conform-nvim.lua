8f07b94dc4f0d78f3afa0aa355ffeed9be417776
Dysthesis <antheoraviel@protonmail.com>
2025-12-26 09:45:39 +1100

    Register formatters for conform-nvim only if they exist

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -18,14 +18,36 @@ require('lz.n').load {
       format_after_save = {
         async = true,
       },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        markdown = { 'markdownlint' },
-        nix = { 'alejandra' },
-        c = { 'clang-format' },
-        rust = { 'rustfmt' },
-        go = { 'go/fmt' },
-      },
+      formatters_by_ft = (function()
+        local function formatters_if_available(entries)
+          local result = {}
+          for _, entry in ipairs(entries) do
+            local formatter = entry[1]
+            local cmd = entry[2] or formatter
+            if vim.fn.executable(cmd) == 1 then
+              table.insert(result, formatter)
+            end
+          end
+          return result
+        end
+
+        local by_ft = {
+          lua = formatters_if_available { { 'stylua' } },
+          markdown = formatters_if_available { { 'markdownlint' } },
+          nix = formatters_if_available { { 'alejandra' } },
+          c = formatters_if_available { { 'clang-format' } },
+          rust = formatters_if_available { { 'rustfmt' } },
+          go = formatters_if_available { { 'go/fmt', 'gofmt' } },
+        }
+
+        for ft, formatters in pairs(by_ft) do
+          if #formatters == 0 then
+            by_ft[ft] = nil
+          end
+        end
+
+        return by_ft
+      end)(),
     }
   end,
 }
