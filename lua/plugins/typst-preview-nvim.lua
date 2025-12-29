e18afa03c6dd6b138f6b16bca3d63e77312e26d7
Dysthesis <antheoraviel@protonmail.com>
2025-12-29 19:08:52 +1100

    Cleanup

--- lua/plugins/typst-preview-nvim.lua
+++
@@ -1,9 +0,0 @@
-require('lz.n').load {
-  'typst-preview.nvim',
-  ft = 'typst',
-  after = function()
-    require('typst-preview').setup {
-      open_cmd = 'zen %s -P typst-preview --class typst-preview',
-    }
-  end,
-}
