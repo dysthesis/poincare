014267c021dd6551a2c4ebc3a3253c664fe061fa
Dysthesis <antheoraviel@protonmail.com>
2026-01-13 17:40:59 +1100

    Go cyclomatic complexity

---
+++ queries/go/complexity.scm
@@ -0,0 +1,13 @@
+; Cyclomatic complexity query for Go.
+
+(function_declaration) @function
+(method_declaration) @function
+
+(if_statement) @decision
+(for_statement) @decision
+(expression_switch_statement) @decision
+(type_switch_statement) @decision
+(select_statement) @decision
+(expression_case) @decision
+(type_case) @decision
+(communication_case) @decision
