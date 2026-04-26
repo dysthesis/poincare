2adcf65358672b2d8b0f0e9d7183f11d4de186e0
Dysthesis <antheoraviel@protonmail.com>
2026-04-27 08:13:16 +1000

    fix(format): avoid double save formatting

--- init.lua
+++ init.lua
@@ -790,19 +790,16 @@ require('lz.n').load {
     after = function()
       require('conform').setup {
         notify_on_error = false,
-        format_on_save = function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for languages that don't
+        format_after_save = function(bufnr)
+          -- Disable LSP fallback for languages that don't
           -- have a well standardized coding style. You can add additional
           -- languages here or re-enable it for the disabled ones.
           local disable_filetypes = {}
           return {
             timeout_ms = 500,
-            lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+            lsp_format = disable_filetypes[vim.bo[bufnr].filetype] and 'never' or 'fallback',
           }
         end,
-        format_after_save = {
-          async = true,
-        },
         formatters_by_ft = {
           lua = { 'stylua' },
           markdown = { 'markdownlint' },
