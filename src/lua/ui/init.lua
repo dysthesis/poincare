3d59e23ff62d13369051a29a8ca2ca7d4005adbc
Dysthesis <antheoraviel@protonmail.com>
2026-09-10 22:45:35 +1000

    completion: transparent menu

--- src/lua/ui/init.lua
+++ src/lua/ui/init.lua
@@ -4,6 +4,17 @@ local wo, g, cmd, opt = vim.wo, vim.g, vim.cmd, vim.o
 g.minimal_transparent = true -- transparent background
 cmd.colorscheme("minimal")
 
+local transparent_pmenu = {
+  "Pmenu",
+  "PmenuKind",
+  "PmenuExtra",
+  "PmenuMatch",
+}
+
+for _, group in ipairs(transparent_pmenu) do
+  vim.cmd(("highlight %s guibg=NONE ctermbg=NONE"):format(group))
+end
+
 opt.conceallevel = 2 -- How much syntax to hide
 wo.relativenumber = true
 opt.colorcolumn = "80"
