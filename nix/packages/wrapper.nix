7a994267070c618fc3140da9ce964b4a56dfb04b
Dysthesis <antheoraviel@protonmail.com>
2026-09-12 11:46:24 +1000

    repo, completion: formatting, use default completion window geometry

--- nix/packages/wrapper.nix
+++ nix/packages/wrapper.nix
@@ -9,69 +9,68 @@
   # Parameters for configuring the final derivation itself.
   name ? "poincare",
   # Plugins that must be eagerly loaded.
-  eagerPlugins ? [ ],
+  eagerPlugins ? [],
   # Plugins that can be lazily loaded.
-  lazyPlugins ? [ ],
+  lazyPlugins ? [],
   # Where the config is located.
   cfgDir ? ../../src,
   ...
-}:
-let
-  inherit (lib)
+}: let
+  inherit
+    (lib)
     getName
     unique
     ;
 
   # Wrap a plugin with its dependencies as well
   withDeps = builtins.foldl' (
-    acc: next: acc ++ [ next ] ++ (withDeps (next.dependencies or [ ]))
-  ) [ ];
+    acc: next: acc ++ [next] ++ (withDeps (next.dependencies or []))
+  ) [];
 
-  packpath =
-    let
-      # Construct symlinks for each plugin to the destination in the packpath
-      linkPlugins =
-        dest: plugins:
-        lib.concatMapStringsSep "\n" (
-          plugin: "ln -vsfT ${plugin} $out/pack/${name}/${dest}/${getName plugin}"
-        ) plugins;
-    in
-    runCommandLocal "packpath" { }
-      # sh
-      ''
-        mkdir -p $out/pack/${name}/{start,opt}
-        cp -r ${../../src}/* $out/
+  packpath = let
+    # Construct symlinks for each plugin to the destination in the packpath
+    linkPlugins = dest: plugins:
+      lib.concatMapStringsSep "\n" (
+        plugin: "ln -vsfT ${plugin} $out/pack/${name}/${dest}/${getName plugin}"
+      )
+      plugins;
+  in
+    runCommandLocal "packpath" {}
+    # sh
+    ''
+      mkdir -p $out/pack/${name}/{start,opt}
+      cp -r ${../../src}/* $out/
 
 
-        ${
-          eagerPlugins
-          |> withDeps
-          |> unique
-          |> linkPlugins "start"
-        }
-        ${
-          lazyPlugins
-          |> withDeps
-          |> unique
-          |> linkPlugins "opt"
-        }
-      '';
+      ${
+        eagerPlugins
+        |> withDeps
+        |> unique
+        |> linkPlugins "start"
+      }
+      ${
+        lazyPlugins
+        |> withDeps
+        |> unique
+        |> linkPlugins "opt"
+      }
+    '';
 in
-symlinkJoin {
-  inherit name;
-  paths = [ neovim-unwrapped ];
-  nativeBuildInputs = [ makeWrapper ];
-  postBuild = ''
-    wrapProgram $out/bin/nvim \
-      --add-flags '-u' \
-      --add-flags '${packpath}/init.lua' \
-      --add-flags '--cmd' \
-      --add-flags "'set packpath^=${packpath} | set runtimepath^=${packpath}'" \
-      --set-default NVIM_APPNAME "${name}"
-  '';
+  symlinkJoin {
+    inherit name;
+    paths = [neovim-unwrapped];
+    nativeBuildInputs = [makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/nvim \
+        --add-flags '-u' \
+        --add-flags '${packpath}/init.lua' \
+        --add-flags '--cmd' \
+        --add-flags "'set packpath^=${packpath} | set runtimepath^=${packpath}'" \
+        --set-default NVIM_APPNAME "${name}"
+    '';
 
-  passthru = {
-    inherit packpath;
-  };
-  meta.mainProgram = "nvim";
-}
+    passthru = {
+      inherit packpath;
+    };
+    meta.mainProgram = "nvim";
+  }
